fbaf5274e23eb0d0b18a1d37cb73d28717531cdd
yash <yashnagazirkar239@gmail.com>
2025-11-23 12:37:22 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+def even_odd(n):
+    if n%==0:
+      return "even"   
+    else:
+       return "odd "
+    
+
+
+def factorial(n):
+   if n==0 or n==1:
+      return 1
+   else:
+      return n *factorial(n-1)
